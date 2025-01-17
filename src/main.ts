7e142a32bb837f894ae4300ae93be38f50579f49
garvit <garvit@wednesday.is>
2025-01-17 15:28:12 +0530

    Add comment 1

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// Comment
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   console.log({app})
